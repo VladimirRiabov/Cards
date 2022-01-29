ad341bbc047f70d4bb6d07b7ea236293ac84f45a
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-29 17:39:01 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Владимир Рябов on 29.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
