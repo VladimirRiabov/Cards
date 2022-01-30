269a3e236f53540aa49a13055f783af0cdc0c136
VladimirRiabov <97258826+VladimirRiabov@users.noreply.github.com>
2022-01-30 13:45:38 +0300

    Creating a rotation gesture

--- Cards/View/ResizableView.swift
+++ Cards/View/ResizableView.swift
@@ -10,25 +10,37 @@ import SwiftUI
 struct ResizableView: View {
     @State private var transform = Transform()
     @State private var previousOffset: CGSize = .zero
+    @State private var previousRotation: Angle = .zero
     // 1
     private let content = RoundedRectangle(cornerRadius: 30.0)
     private let color = Color.red
     var body: some View {
+        let rotationGesture = RotationGesture()
+            .onChanged { rotation in
+                transform.rotation += rotation - previousRotation
+                previousRotation = rotation
+            }
+            .onEnded { _ in
+                previousRotation = .zero
+            }
         let dragGesture = DragGesture()
-          .onChanged { value in
-            transform.offset = value.translation + previousOffset
-          }
-          .onEnded { _ in
-            previousOffset = transform.offset
-          }
+            .onChanged { value in
+                transform.offset = value.translation + previousOffset
+            }
+            .onEnded { _ in
+                previousOffset = transform.offset
+            }
         // 2
         content
             .frame(
-             width: transform.size.width,
-             height: transform.size.height)
+                width: transform.size.width,
+                height: transform.size.height)
             .foregroundColor(color)
+            
+            .rotationEffect(transform.rotation)
             .offset(transform.offset)
            .gesture(dragGesture)
+           .gesture(rotationGesture)
     }
     
 }
